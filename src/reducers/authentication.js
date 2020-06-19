41437f4985b601aa260d85741d3097ae901a5749
FelipeEnne <felipeenne@gmail.com>
2020-06-19 18:32:45 -0300

    create reducers

---
+++ src/reducers/authentication.js
@@ -0,0 +1,31 @@
+import {
+    LOGIN_REQUEST, 
+    LOGIN_SUCCESS, 
+    LOGIN_FAILURE,
+    LOGOUT,
+} from '../helper/constant'
+
+
+let user = JSON.parse(localStorage.getItem('user'));
+const initialState = user ? { loggedIn: true, user } : {};
+
+export function authentication(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        loggingIn: true,
+        user: action.user
+      };
+    case LOGIN_SUCCESS:
+      return {
+        loggedIn: true,
+        user: action.user
+      };
+    case LOGIN_FAILURE:
+      return {};
+    case LOGOUT:
+      return {};
+    default:
+      return state
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,18 @@ import { createLogger } from 'redux-logger';
 import { combineReducers } from 'redux';
 
 import { users } from './users'
+import { alert } from './alert'
+import { authentication } from './authentication'
+import { registration } from './registration'
+
 
 const loggerMiddleware = createLogger();
 
 const rootReducer = combineReducers({
     users,
+    alert,
+    authentication,
+    registration,
 });
 
 export const store = createStore(
